7bb746ae72f49f2aa85d0b6c51960d06a98769da
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-10-25 19:26:30 +0530

    Initial commit

---
+++ src/main/java/com/openmrs/OpenmrsContribFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.openmrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmrsContribFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenmrsContribFlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openmrs/OpenmrsContribFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenmrsContribFlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
